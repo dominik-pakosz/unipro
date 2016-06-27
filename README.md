6db82f8564a97b9ae24efe2b437300e92c1d9d6b
Pakoo <dpakoo906@gmail.com>
2016-06-27 16:52:47 +0200

    poczatek prac

---
+++ README.md
@@ -0,0 +1,4 @@
+unipro
+======
+
+A Symfony project created on June 27, 2016, 12:16 pm.
---
+++ src/unipro/PageBundle/PageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace unipro\PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PageBundle extends Bundle
+{
+}
